97324d1f864a8cdf4a3d91632fbb80510c17ab81
jjoy-joooo <150677036+jjoy-joooo@users.noreply.github.com>
2024-02-20 18:14:08 +0900

    Update main.py
    변경 main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "마라탕먹으러 가야쥐"}
+    return {"Hello": "마라탕먹으러 가야쥐..!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
